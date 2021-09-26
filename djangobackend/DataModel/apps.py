e9e4308d1e9fd74e790fb8b193170bce8a6a3681
karthikeyansuku <78297832+karthikeyansuku@users.noreply.github.com>
2021-09-26 13:06:14 +0530

    changes

--- djangobackend/DataModel/apps.py
+++ djangobackend/DataModel/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
 
 
-class AppusersConfig(AppConfig):
+class AppUserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appusers'
+    name = 'AppUser'
+class StationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Station'
+class BookingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Booking'
