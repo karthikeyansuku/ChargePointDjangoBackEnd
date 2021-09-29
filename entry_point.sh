14d6cfaa60c0238afba93f278540077876af9de9
karthikeyansuku <78297832+karthikeyansuku@users.noreply.github.com>
2021-09-29 14:25:04 +0530

    commit

--- entry_point.sh
+++ entry_point.sh
@@ -2,6 +2,7 @@
 python manage.py shell
 python manage.py makemigrations
 python manage.py migrate
+python manage.py collectstatic
 
 echo 'Django is migrated with tables and relations'
 python manage.py runserver 0.0.0.0:8000
